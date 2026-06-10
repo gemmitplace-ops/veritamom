29ff628cb248ef7906ab6a0065ffbaca4fe9da6b
gemmitplace-ops <gemmitplace-ops@users.noreply.github.com>
2026-06-10 17:24:24 +0800

    fix: simplify entrypoint to just start server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-set -e
-
-echo "Running database migrations..."
-node ./node_modules/.bin/prisma db push --schema=prisma/schema.prisma --skip-generate
-
-echo "Starting server..."
 exec node server.js
